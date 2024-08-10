fcb028ce9dc0b8d9ad52f0f7c397bedb7cfb297b
NIKESH KUMAR <nikesh.kumar7@ibm.com>
2024-08-10 16:04:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,129 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <img src="../src/assets/image.png" />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            margin: "auto",
+          }}
+        >
+          <b>✒️ GST: 07BEGPP9390R1ZD</b>
+          <b>🖋️EPF: DSNHP2736227000</b>
+          <b> ✏️ESIC: 11001413650000999</b>
+          <b> ✒️PAN: BEGPP9390R</b>
+        </div>
+      </div>
+      <div>
+        <h2 style={{ color: "green" }}>INTRODUCTION 📝 </h2>
+        <p>
+          We would like to introduce ourselves as a leading ϐirm of Engineers
+          and Technical hands for erection and commissioning of all types of
+          works in telecom sector. We have modern tools, tackles and equipment’s
+          for execution of all types of jobs like. We have a team of qualiϐied
+          and experienced engineers having an excellent knowledge in this ϐield.
+          We have undertaken all types of working as contractors with leading
+          companies Cable Trenching and OFC Trenching & Ducting, HDD, Cable
+          Pulling, Blowing and Splicing We are drivers and engineers to impart
+          technical requirements and support to your prestigious organization
+          and please send your enquiries of whatsoever that are connected to
+          telecom work.{" "}
+        </p>
+      </div>
+      <div>
+        <h2 style={{ color: "green" }}>📖 ABOUT COMPANY 📖 </h2>
+        <p>
+          We would like to introduce ourselves as a leading ϐirm of Engineers
+          and Technical hands for erection and commissioning of all types of
+          works in telecom sector. We have modern tools, tackles and equipment’s
+          for execution of all types of jobs like. We have a team of qualiϐied
+          and experienced engineers having an excellent knowledge in this ϐield.
+          We have undertaken all types of working as contractors with leading
+          companies Cable Trenching and OFC Trenching & Ducting, HDD, Cable
+          Pulling, Blowing and Splicing We are drivers and engineers to impart
+          technical requirements and support to your prestigious organization
+          and please send your enquiries of whatsoever that are connected to
+          telecom work.
+        </p>
+      </div>
+      <div>
+        <h2 style={{ color: "green" }}>⚒️ WORKING FIELD ⚒️</h2>
+        <div
+          style={{
+            display: "flex",
+            gap: "10rem",
+            alignItems: "flex-start", // Aligns all children to the start
+          }}
+        >
+          <div
+            style={{
+              background: "#8d948d54",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              padding: "1rem",
+            }}
+          >
+            <p>➡️ Construction of Cable Trenches</p>
+            <p>➡️ OFC Trenching & Ducting</p>
+            <p>➡️ Cable Pulling</p>
+          </div>
+          <div
+            style={{
+              background: "#8d948d54",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              padding: "2rem",
+            }}
+          >
+            <p>➡️ Fiber Maintenance</p>
+            <p>➡️ Blowing, Splicing and IBD Work</p>
+          </div>
+          <div
+            style={{
+              background: "#8d948d54",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              padding: "2rem",
+            }}
+          >
+            <p>➡️ Aerial Cabling</p>
+            <p>➡️ Mid Spice Branching</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="">
+        <h2 style={{ color: "green" }}>♟️ EXPERIENCE AND PERFORMANCE ♟️ </h2>
+        <b>Tata Communication Ltd</b>
+      </div>
+      <div className="">
+        <h2 style={{ color: "green" }}>🙅‍♂️ TEAM WORK 🙅‍♂️ </h2>
+        <p>
+          <b>M/s Telecon Enterprises : </b> from the beginning of the
+          organization we were not depending on individual’s excellence, we
+          always depending on Team Work, teamwork is the ability to work
+          together toward a common vision. The ability to direct individual
+          accomplishments towards the organizational objectives. It is the fuel
+          that allows common people to attain uncommon results. None of us,
+          including me, ever do great things. But we can all do small things,
+          with great desire, and together we can do something wonderful.{" "}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
